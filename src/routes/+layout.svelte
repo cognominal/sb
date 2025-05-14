e79e64f9756e889eff88d930f1fc41ea31295af6
Stéphane Payrard <cognominal@gmail.com>
2025-05-15 01:04:07 +0200

    navbar
    some default styling.
    Why does it work without global ?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,15 @@
 	let { children } = $props();
 </script>
 
+<nav class="w-full">
+  <div class="flex flex-row gap-4 items-center w-full">
+    <div><a href="/" class="text-gray-800 hover:text-blue-600">home</a></div>
+    <div><a href="/docs" class="text-gray-800 hover:text-blue-600">docs</a></div>
+    <div class="ml-auto">
+      <a href="https://github.com/cognominal/sb">
+        <img src="github-mark/github-mark.svg" class="h-6 sm:h-8 w-auto" alt="GitHub Logo" />
+      </a>
+    </div>
+  </div>
+</nav>
 {@render children()}
