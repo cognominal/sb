1a952677f069568b5ced0bea58cd341551e35ee2
Stéphane Payrard <cognominal@gmail.com>
2025-05-19 02:00:00 +0200

    [layout.svelte] checkbox to hide experimental or admin entries

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,21 +2,28 @@
 	import '../app.css';
 
 	let { children } = $props();
+
+	let experimental = $state(false);
 </script>
 
 <nav class="w-full">
 	<div class="flex w-full flex-row items-center gap-4">
 		<div><a href="/" class="text-gray-800 hover:text-blue-600">home</a></div>
 		<div><a href="/docs" class="text-gray-800 hover:text-blue-600">docs</a></div>
-		<div><a href="/admin" class="text-gray-800 hover:text-blue-600">admin</a></div>
-		<div><a href="/create-doc" class="text-gray-800 hover:text-blue-600">create-doc</a></div>
-		<div><a href="/attach-test" class="text-gray-800 hover:text-blue-600">attach-test</a></div>
-
-		<div class="ml-auto">
-			<a href="https://github.com/cognominal/sb">
-				<img src="/github-mark/github-mark.svg" class="h-6 w-auto sm:h-8" alt="GitHub Logo" />
-			</a>
-		</div>
+		{#if experimental}
+			<div><a href="/admin" class="text-gray-800 hover:text-blue-600">admin</a></div>
+			<div><a href="/create-doc" class="text-gray-800 hover:text-blue-600">create-doc</a></div>
+			<div><a href="/attach-test" class="text-gray-800 hover:text-blue-600">attach-test</a></div>
+		{/if}
+	   <div class="ml-auto flex flex-row items-center gap-4">
+		   <label class="flex items-center gap-1">
+			   <input type="checkbox" bind:checked={experimental} />
+<span class="text-sm text-gray-400">experimental</span>
+		   </label>
+		   <a href="https://github.com/cognominal/sb">
+			   <img src="/github-mark/github-mark.svg" class="h-6 w-auto sm:h-8" alt="GitHub Logo" />
+		   </a>
+	   </div>
 	</div>
 </nav>
 {@render children()}
